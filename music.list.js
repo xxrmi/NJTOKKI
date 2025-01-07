145db82d50861fac93726fc578c3d934d776c0a3
xxrmi <133154906+xxrmi@users.noreply.github.com>
2025-01-07 16:47:03 -0500

    Add files via upload

---
+++ music.list.js
@@ -0,0 +1,85 @@
+let allMusic = [
+
+{
+    name: "Hype Boy - New Jeans EP1.",
+    artist: "New Jeans",
+    img: "bunn.jpg",
+    src: "hype",
+},
+{
+    name: "Attention - New Jeans EP.1",
+    artist: "New Jeans",
+    img: "bunn.jpg",
+    src: "attention",
+},
+{
+    name: "Cookie - New Jeans EP.1",
+    artist: "New Jeans",
+    img: "bunn.jpg",
+    src: "cookie",
+},
+{
+    name: "Hurt - New Jeans EP.1",
+    artist: "New Jeans",
+    img: "bunn.jpg",
+    src: "hurt",
+},
+{
+    name: "OMG - New Jeans Single",
+    artist: "New Jeans",
+    img: "bunn2.jpg",
+    src: "OMG",
+},
+{
+    name: "Ditto - New Jeans Single",
+    artist: "New Jeans",
+    img: "bunn3.jpg",
+    src: "ditto",
+},
+{
+    name: "Super Shy - Get Up EP.2",
+    artist: "New Jeans",
+    img: "gg.png",
+    src: "supershy",
+},
+{
+    name: "New Jeans - Get Up EP.2",
+    artist: "New Jeans",
+    img: "gg.png",
+    src: "newjeans",
+},
+{
+    name: "ETA - Get Up EP.2",
+    artist: "New Jeans",
+    img: "gg.png",
+    src: "ETA",
+},
+{
+    name: "Get Up - Get Up EP.2",
+    artist: "New Jeans",
+    img: "gg.png",
+    src: "getup",
+},
+{
+    name: "Cool With You - Get Up EP.2",
+    artist: "New Jeans",
+    img: "gg.png",
+    src: "coolwithyou",
+},
+{
+    name: "ASAP - Get Up EP.2",
+    artist: "New Jeans",
+    img: "gg.png",
+    src: "ASAP",
+},
+
+//To add more songs, just copy the following code and paste inside the array//
+//
+// {
+//  name: "New Jeans Ep.1 - Hurt",
+//  artist: "New Jeans",
+//  img: "bunn.jpg",
+//  src: "hurt.mp3",
+// },
+
+]
